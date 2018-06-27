a4259873e2f10740acceb7cab92148ba89a6fc07
Vipul Kuruppu <vipul@Asterix.local>
2018-06-28 02:01:47 +0800

    updating method - Thu 28 Jun 2018 02:01:47 +08

--- src/uob/jenkins/shared/common/ResourcePropertyReader.groovy
+++ src/uob/jenkins/shared/common/ResourcePropertyReader.groovy
@@ -9,14 +9,20 @@ package uob.jenkins.shared.common
         //def propertyFileResource = libraryResource 'slaves/CLSTSG80/slave.properties'
         //def config = new ConfigSlurper(property).parse(new File(propertyFile).toURL())
         println "Starting ResourcePropertyReader.getPropertyValue()...."
-        def properties = new Properties()
-        def propertiesFile = new File(propertyFile)
-        properties.load(propertiesFile.newDataInputStream())
-        def config = new ConfigSlurper().parse(properties)
+        //def properties = new Properties()
+        //def propertiesFile = new File(propertyFile)
+        //properties.load(propertiesFile.newDataInputStream())
+        //def config = new ConfigSlurper().parse(properties)
+        
+        def props = new Properties()
+        new File(propertyFile).withInputStream { 
+        stream -> props.load(stream) 
+        }
         
         println "Finishing ResourcePropertyReader.getPropertyValue()...."
         //return config."${property}"
-        return "ownclssg"
+        return props[property]
+        //return "ownclssg"
 
     }
 
