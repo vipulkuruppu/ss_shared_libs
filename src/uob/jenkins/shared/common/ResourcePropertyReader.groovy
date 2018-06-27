54330a92d2a0b493abf5cb5bfe8d1b29bf788f2d
Vipul Kuruppu <vipul@Asterix.local>
2018-06-28 04:01:49 +0800

    updating method - Thu 28 Jun 2018 04:01:49 +08

--- src/uob/jenkins/shared/common/ResourcePropertyReader.groovy
+++ src/uob/jenkins/shared/common/ResourcePropertyReader.groovy
@@ -9,14 +9,13 @@ String getPropertyValue(String propertyFile, String property){
 
     def propertyFileResource = libraryResource propertyFile
     def props = new Properties()
-    //def propFile = new File(propertyFileResource)
     def stream = new ByteArrayInputStream(propertyFileResource.getBytes())
     props.load(new DataInputStream(stream))
-    def config = new ConfigSlurper("${property}").parse(props)
+    //def config = new ConfigSlurper("${property}").parse(props)
+    def config = new ConfigSlurper().parse((new Properties()).load(new DataInputStream(new ByteArrayInputStream(propertyFileResource.getBytes()))))
 
     println "Finishing ResourcePropertyReader.getPropertyValue()...."
     return config."${property}"
-    //return propertyFileResource
 
 }
 return this
